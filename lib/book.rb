569f980ccb3227e47472cdf250f6dc653adbea79
pauline-njunguna <paulinenjunguna.w@gmail.com>
2023-02-08 23:18:42 +0300

    Done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
+class book
+    attr_reader :title 
+    attr_accessor :author_name :page_count, :genre
 
+    def initialize(title)
+      @title = title
+    end
+    def turn_page 
+        puts "page turned"
+    end
+end
 
